8d7f3d0cd38b938176905f9814421d1f732bed88
rapilap <raffyabdillah03@gmail.com>
2025-09-01 10:44:21 +0700

    update: log in (err)

--- backend/src/routes/userRoute.js
+++ backend/src/routes/userRoute.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const { registerUser } = require("../controllers/userController");
+const authMiddleware = require("../middleware/authMiddleware");
+const { loginUser } = require("../controllers/authController");
 const router = express.Router();
 
 router.post("/register", registerUser);
+router.post("/login", loginUser);
 
 module.exports = router;
--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -2,31 +2,58 @@
   <div class="auth-container">
     <div class="auth-box">
       <h2 class="title">Login</h2>
-      <form @submit.prevent="handleLogin">
-        <div class="form-group">
-          <label>Email</label>
-          <input type="email" v-model="email" placeholder="Masukkan email" required />
-        </div>
-        <div class="form-group">
-          <label>Password</label>
-          <input type="password" v-model="password" placeholder="Masukkan password" required />
-        </div>
-        <router-link to="/dashboard" class="btn">Login</router-link>
-      </form>
-      <p class="switch">Belum punya akun? <router-link to="/register">Register</router-link></p>
+      <!-- <form @submit.prevent="handleLogin"> -->
+      <div class="form-group">
+        <label>Email</label>
+        <input
+          type="email"
+          v-model="email"
+          placeholder="Masukkan email"
+          required
+        />
+      </div>
+      <div class="form-group">
+        <label>Password</label>
+        <input
+          type="password"
+          v-model="password"
+          placeholder="Masukkan password"
+          required
+        />
+      </div>
+      <!-- <router-link to="/dashboard" class="btn">Login</router-link> -->
+      <button class="btn" @click="handleLogin">Login</button>
+      <!-- </form> -->
+      <p class="switch">
+        Belum punya akun? <router-link to="/register">Register</router-link>
+      </p>
     </div>
   </div>
 </template>
 
 <script setup>
 import { ref } from "vue";
+import { useRouter } from "vue-router";
 
 const email = ref("");
 const password = ref("");
+const router = useRouter();
+const message = ref("");
 
-const handleLogin = () => {
-  alert(`Login dengan email: ${email.value}`);
-  email.value = password.value = "";
+const handleLogin = async () => {
+  try {
+    const res = await api.post("/login", {
+      email: email.value,
+      password: password.value,
+    });
+    localStorage.setItem("token", res.data.token);
+    router.push("/dashboard");
+  } catch (error) {
+    message.value = error.response?.data?.message || "Login gagal!";
+    alert(message);
+  }
+  // alert(`Login dengan email: ${email.value}`);
+  // email.value = password.value = "";
 };
 </script>
 
@@ -43,7 +70,7 @@ const handleLogin = () => {
   color: white;
   padding: 40px;
   border-radius: 12px;
-  box-shadow: 0 4px 12px rgba(0,0,0,0.3);
+  box-shadow: 0 4px 12px rgba(0, 0, 0, 0.3);
   width: 100%;
   max-width: 380px;
 }
