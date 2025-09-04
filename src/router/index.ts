32c4b3699b3807983baaeb4da7eee306f3d047ed
rapilap <raffyabdillah03@gmail.com>
2025-09-04 11:44:29 +0700

    update: create product

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,31 +1,33 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../pages/Home.vue'
-import Shop from '../pages/Shop.vue'
-import ProductDetail from '../pages/ProductDetail.vue'
-import About from '../pages/About.vue'
-import Contact from '../pages/Contact.vue'
-import Login from '../pages/Login.vue'
-import Register from '../pages/Register.vue'
-import Dashboard from '../pages/Dashboard.vue'
-import Cart from '../pages/Cart.vue'
-import Checkout from '../pages/Checkout.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "../pages/Home.vue";
+import Shop from "../pages/Shop.vue";
+import ProductDetail from "../pages/ProductDetail.vue";
+import About from "../pages/About.vue";
+import Contact from "../pages/Contact.vue";
+import Login from "../pages/Login.vue";
+import Register from "../pages/Register.vue";
+import Dashboard from "../pages/Dashboard.vue";
+import Cart from "../pages/Cart.vue";
+import Checkout from "../pages/Checkout.vue";
+import TesEndpoint from "../pages/TesEndpoint.vue";
 
 const routes = [
-    { path: '/', component: Home },
-    { path: '/shop', component: Shop },
-    { path: '/product/:id', component: ProductDetail },
-    { path: '/about', component: About },
-    { path: '/contact', component: Contact },
-    { path: '/cart', component: Cart },
-    { path: '/checkout', component: Checkout },
-    { path: '/login', component: Login },
-    { path: '/register', component: Register },
-    { path: '/dashboard', component: Dashboard }
-]
+  { path: "/", component: Home },
+  { path: "/shop", component: Shop },
+  { path: "/product/:id", component: ProductDetail },
+  { path: "/about", component: About },
+  { path: "/contact", component: Contact },
+  { path: "/cart", component: Cart },
+  { path: "/checkout", component: Checkout },
+  { path: "/login", component: Login },
+  { path: "/register", component: Register },
+  { path: "/dashboard", component: Dashboard },
+  { path: "/tes", component: TesEndpoint },
+];
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes
-})
+  history: createWebHistory(),
+  routes,
+});
 
-export default router
+export default router;
