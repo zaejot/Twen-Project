2f9bcb826f3411c1ef225b39be60e5878a49da1d
rapilap <raffyabdillah03@gmail.com>
2025-09-05 13:25:48 +0700

    update: fix navbar

--- backend/src/routes/userRoute.js
+++ backend/src/routes/userRoute.js
@@ -4,10 +4,7 @@ const authMiddleware = require("../middleware/authMiddleware");
 const { loginUser } = require("../controllers/authController");
 const router = express.Router();
 
-import Dashboard from './pages/Dashboard.vue';
-import AddProduct from './pages/AddProduct.vue';
-
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 
-module.exports = router;    
+module.exports = router;
